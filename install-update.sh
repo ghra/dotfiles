4cb01b8ce309f18672eaed0ecaa7f11a8708ea25
Adam Mazur <ghr@o2.pl>
2013-11-16 00:02:57 +0100

    install and/or update script - just run

---
+++ install-update.sh
@@ -0,0 +1,46 @@
+
+
+
+function install_or_update ()
+{
+	SRC_FILE=$1
+	DST_FILE=$2
+
+	# if destination file does not exists - do a link or do a copy
+	if [[ ! -e "$DST_FILE" ]]; then
+		ln -v "$SRC_FILE" "$DST_FILE" || cp -a -v "$SRC_FILE" "$DST_FILE"
+		return
+	fi
+
+	# if this is hard link - nothing to do :)
+	src_inode=`stat -c '%D %i' "$SRC_FILE"`
+	dst_inode=`stat -c '%D %i' "$DST_FILE"`
+	if [[ "$src_inode" = "$dst_inode" ]]; then
+		echo "$SRC_FILE -> $DST_FILE: was a hard link"
+		return
+	fi
+
+	# if destination file exists and is not a hard link - check if the same or copy with backup
+	if diff -q "$SRC_FILE" "$DST_FILE"; then
+		echo "$SRC_FILE -> $DST_FILE: is the same"
+	else
+		cp -v -a -b "$SRC_FILE" "$DST_FILE"
+	fi
+}
+
+
+for file in \
+	git-flow-completion.bash \
+	git-completion.sh \
+	git-prompt.sh \
+	bashrc_private
+do
+	install_or_update "${file}" "${HOME}/.${file}";
+done
+
+if grep -q "source ~/.bashrc_private" "${HOME}/.bashrc"; then
+	echo "~/.bashrc still loading ~/.bashrc_private"
+else
+	echo "updating ~/.bashrc to load ~/.bashrc_private"
+	echo "source ~/.bashrc_private" >>~/.bashrc
+fi
